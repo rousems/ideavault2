43114188b365cd56f889c59de2fc35d16b73c3c6
rousems <rousems@rose-hulman.edu>
2020-10-19 15:05:18 -0400

    adding files

---
+++ public/scripts/main.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview
+ * Provides the JavaScript interactions for all pages.
+ *
+ * @author 
+ * MATTHEW ROUSE
+ */
+
+/** namespace. */
+var rhit = rhit || {};
+
+
+
+
+/* Main */
+/** function and class syntax examples */
+rhit.main = function () {
+	console.log("Ready");
+	
+};
+
+rhit.main();
